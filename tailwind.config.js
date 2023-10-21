286907351447f32c5145928621653963cd74418f
Ethan Wilson <E.Wilson17@newcastle.ac.uk>
2023-10-21 14:38:06 +0100

    TailwindCSS config

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+
+export default {
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+],
+
+theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'white': '#ffffff',
+      'black': '#000000',
+      'red': '#FF0000',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+      nunito: ['Nunito']
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+
+plugins: [forms],
+}
+
